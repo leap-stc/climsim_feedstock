950eae583999de1f7b92ee473d22c88697af7ad3
Julius Busecke <julius@ldeo.columbia.edu>
2024-06-05 12:00:56 -0400

    Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -208,10 +208,10 @@ climsim_lowres_mli = (
     beam.Create(lowres_mli_pattern.items())
     | CheckpointFileTransfer(
         transfer_target=cache_target,
-        max_executors=25,
+        max_executors=50,
         concurrency_per_executor=10,
         initial_backoff=3.0,
-        fsspec_sync_patch=False,# works but is slow. Testing with fsspec and new backoff retry
+        fsspec_sync_patch=True,# works but is slow. Testing with fsspec and new backoff retry
         )
     | OpenURLWithFSSpec(cache=None, fsspec_sync_patch=True)
     | OpenWithXarray(
